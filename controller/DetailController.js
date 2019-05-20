958741f8d71873c0ab84d9dad66e4269327263c7
kimvietanh <anhkv@sphinx-software.com>
2019-05-20 10:00:00 +0700

    project Updated

--- controller/DetailController.js
+++ controller/DetailController.js
@@ -1,8 +1,10 @@
 class DetailController {
 
     async getDetail(ctx, next) {
-        //to do
-        ctx.render('detail.html');
+        let product = await ctx.productRepository.findProductById(ctx.query.id);
+        let id_type = product.id_type;
+        let products = await ctx.productRepository.findProductByIdType(id_type);
+        ctx.render('detail.html', {product, products});
     }
 
 }
